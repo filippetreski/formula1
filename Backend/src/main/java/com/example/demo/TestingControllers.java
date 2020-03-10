e74705156ae39aa112a7d3c6aeb863bc3ed6c780
Todorovikj <necotodorovik@gmail.com>
2020-03-10 20:50:55 +0100

    Added apache jenna.

--- Backend/src/main/java/com/example/demo/TestingControllers.java
+++ Backend/src/main/java/com/example/demo/TestingControllers.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-
+import org.apache.jena.rdf.model.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/home")
 public class TestingControllers {
 
+    public Model modelExample = ModelFactory.createDefaultModel();
     @GetMapping()
     public ResponseEntity<String> home(){
         return ResponseEntity.ok("Home endpoint is working good!");
